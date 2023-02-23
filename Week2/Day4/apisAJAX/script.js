1e8a10ac1eb9c8aff0df84aa3d49d97dd12c075b
Spencer Rauch <rauchs@gmail.com>
2023-02-23 09:42:27 -0800

    poke api stuff

---
+++ Week2/Day4/apisAJAX/script.js
@@ -0,0 +1,30 @@
+console.log("linked")
+var url = "https://pokeapi.co/api/v2/pokemon/"
+
+var pokeInputElement = document.querySelector("#pokeNameInput")
+var resultsElement = document.querySelector("#results")
+
+
+async function getPoke(){
+    var searchTerm = pokeInputElement.value
+    console.log(searchTerm)
+    var response = await fetch(url+searchTerm)
+    var data = await response.json()
+    console.log(data)
+    resultsElement.innerHTML = `
+    <img src="${data.sprites.front_default}" alt="${data.name}" width="100px">
+    `
+    
+}
+
+async function getRand(){
+    var random = Math.floor(Math.random()*999);
+    var response = await fetch(url+random)
+    var data = await response.json()
+    console.log(data)
+    resultsElement.innerHTML = `
+        <h1>${data.name} </h1>
+        <img src="${data.sprites.front_default}" alt="${data.name}" width="300px">
+    `
+
+}
